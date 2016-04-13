fbab92af373ee50e99d81502e4b6ca872b64ec1e
Siméon Blanc <simeon.blanc@gmail.com>
2016-04-13 07:39:30 +0200

    New preprocessor feature to iterate over values
    This is especially important when we have to do the same thing
     with different input.

--- preproc.py
+++ preproc.py
@@ -5,6 +5,12 @@ if __name__ == '__main__':
     import re
     import sys
 
+    def inject_variable(line, **variables):
+
+        for key, value in variables.iteritems():
+            line = line.replace('{{%s}}' % key, value)
+        return line
+
     class SyntaxException(Exception):
         pass
 
@@ -17,49 +23,68 @@ if __name__ == '__main__':
     try:
         with open(sys.argv[1], 'r') as f:
 
-            waiting_lines = [(1, [])]
+            waiting_lines = [(1, None, [])]
 
             for no, line in enumerate(f):
 
                 # is it a macro line?
                 cleaned_line = line.strip()
 
-                # Is it a macro line ?
-                #   #loop{ENV}
-                #   #end
-
-                begin_block = re.match('\s*#begin{(?P<env>\w+)}\s*', cleaned_line)
+                begin_block_count = re.match('^\s*#begin{\s*(?P<macro>[^}]+)\s*}\s*$', cleaned_line)
                 end_block = re.match('\s*#end\s*', cleaned_line)
 
-                if begin_block is not None:
+                if begin_block_count is not None:
 
-                    varname = begin_block.groupdict()['env']
+                    varname = begin_block_count.groupdict()['macro']
 
-                    if varname not in os.environ:
-                        raise SyntaxException("Invalid environment variable: %s" % varname)
+                    if ',' in varname:
+                        variables = varname.split(',')
 
-                    try:
-                        nbvar = int(os.environ[varname])
-                    except ValueError as e:
-                        raise SyntaxException("Invalid variable value: %s (value= '%s')" % (varname, os.environ[varname]))
+                        myvar = variables[0].strip()
+                        values = map(lambda x : x.strip(), variables[1:])
 
-                    waiting_lines.append((nbvar, []))
+                        waiting_lines.append((1, (myvar, values), []))
+                    else:
+                        if varname not in os.environ:
+                            raise SyntaxException("Invalid environment variable: %s" % varname)
+
+                        try:
+                            nbvar = int(os.environ[varname])
+                        except ValueError as e:
+                            raise SyntaxException("Invalid variable value: %s (value= '%s')" % (varname, os.environ[varname]))
+
+                        waiting_lines.append((nbvar, None, []))
 
                 elif end_block is not None:
 
                     if len(waiting_lines) <= 1:
                         raise SyntaxException("Invalid end block (line: %d)" % (no+1))
 
-                    last_pos = waiting_lines.pop()
+                    nbvars, envs, lines = waiting_lines.pop()
 
-                    to_add = last_pos[0] * last_pos[1]
-                    waiting_lines[-1][1].extend(to_add)
+                    to_add = nbvars * lines
+
+                    # we have to add the variable names
+                    if envs is not None:
+                        to_add_converted = []
+                        var = envs[0]
+
+                        to_add_converted = []
+                        for var_value in envs[1]:
+                            for line in to_add:
+                                to_add_converted.append(inject_variable(line, **{var: var_value}))
+
+                        to_add = to_add_converted
+
+                    waiting_lines[-1][2].extend(to_add)
 
                 else:
-                    waiting_lines[-1][1].append(line)
+                    waiting_lines[-1][2].append(line)
 
-            assert len(waiting_lines) == 1
-            sys.stdout.write(''.join(waiting_lines[0][0] * waiting_lines[0][1]))
+            if len(waiting_lines) != 1:
+                raise SyntaxException("A block is not open or is not closed")
+            # We know that the last block is not about a macro
+            sys.stdout.write(''.join(waiting_lines[0][0] * waiting_lines[0][2]))
 
     except SyntaxException as e:
         sys.stderr.write('%s\n' % e)
