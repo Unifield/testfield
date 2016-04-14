dbdb2c860c3932b15bf011cb62c38e5a60649412
Hector Davensburg <hector.davensburg@gmail.com>
2016-04-14 17:43:52 +0200

    New iterators to use "real" data coming from the database

--- preproc.py
+++ preproc.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from credentials import *
+
 if __name__ == '__main__':
     import os
     import re
     import sys
+
+    def get_sql_query(sqlquery):
+        import psycopg2
+        import psycopg2.extras
+
+        conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (DB_ADDRESS, DB_NAME, DB_USERNAME, DB_PASSWORD))
+        cr = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        cr.execute(sqlquery)
+        return cr.fetchall()
+
+    def get_articles(count):
+        return get_sql_query("SELECT default_code AS CODE, name_template AS NAME FROM product_product WHERE batch_management = 'f' AND perishable = 'f'LIMIT %d" % count)
 
     def inject_variable(line, **variables):
 
@@ -23,27 +38,43 @@ if __name__ == '__main__':
     try:
         with open(sys.argv[1], 'r') as f:
 
-            waiting_lines = [(1, None, [])]
+            waiting_lines = [(1, None, [], None)]
 
             for no, line in enumerate(f):
 
                 # is it a macro line?
                 cleaned_line = line.strip()
 
+
+                iterate_over = re.match('^\s*#loop{\s*(?P<kindof>[^} ),]+)\s*,\s*(?P<varname>[^} ),]+)\s*}\s*$', cleaned_line)
                 begin_block_count = re.match('^\s*#begin{\s*(?P<macro>[^}]+)\s*}\s*$', cleaned_line)
                 end_block = re.match('\s*#end\s*', cleaned_line)
 
-                if begin_block_count is not None:
+                if iterate_over is not None:
+                    values = iterate_over.groupdict()
+                    varname = values['varname']
+
+                    try:
+                        number = int(os.environ[varname])
+                    except ValueError as e:
+                        raise SyntaxException("Invalid variable value: %s (value= '%s')" % (varname, os.environ[varname]))
+                    kindof = values['kindof'].upper()
+
+                    assert kindof in ["PRODUCTS"]
+                    waiting_lines.append((1, None, [], get_articles(number)))
+
+                elif begin_block_count is not None:
 
                     varname = begin_block_count.groupdict()['macro']
 
+                    # do we have to iterate over values?
                     if ',' in varname:
                         variables = varname.split(',')
 
                         myvar = variables[0].strip()
                         values = map(lambda x : x.strip(), variables[1:])
 
-                        waiting_lines.append((1, (myvar, values), []))
+                        waiting_lines.append((1, (myvar, values), [], None))
                     else:
                         if varname not in os.environ:
                             raise SyntaxException("Invalid environment variable: %s" % varname)
@@ -53,14 +84,14 @@ if __name__ == '__main__':
                         except ValueError as e:
                             raise SyntaxException("Invalid variable value: %s (value= '%s')" % (varname, os.environ[varname]))
 
-                        waiting_lines.append((nbvar, None, []))
+                        waiting_lines.append((nbvar, None, [], None))
 
                 elif end_block is not None:
 
                     if len(waiting_lines) <= 1:
                         raise SyntaxException("Invalid end block (line: %d)" % (no+1))
 
-                    nbvars, envs, lines = waiting_lines.pop()
+                    nbvars, envs, lines, variables = waiting_lines.pop()
 
                     to_add = nbvars * lines
 
@@ -76,7 +107,15 @@ if __name__ == '__main__':
 
                         to_add = to_add_converted
 
-                    waiting_lines[-1][2].extend(to_add)
+
+                    if variables is not None:
+                        to_add_with_variables = []
+                        for varset in variables:
+                            to_add_with_variables.extend([inject_variable(l, **varset) for l in to_add])
+                    else:
+                        to_add_with_variables = to_add
+
+                    waiting_lines[-1][2].extend(to_add_with_variables)
 
                 else:
                     waiting_lines[-1][2].append(line)
