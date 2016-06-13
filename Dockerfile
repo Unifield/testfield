e9ab22ebcc12c88578d0e5443f7aed3fad9a0671
Hector Davensburg <hector.davensburg@gmail.com>
2016-06-13 16:02:53 +0200

    New docker image to use another user as root

--- Dockerfile
+++ Dockerfile
@@ -1,61 +1,41 @@
-FROM ubuntu
+FROM ubuntu:xenial
 MAINTAINER hectord
 
 WORKDIR /root
 
-RUN apt-get update
-RUN apt-get install -y tmux libxml2-dev libxslt1-dev python-dev libjpeg-dev libpng-dev libfreetype6-dev build-essential xvfb firefox wget pkg-config libpq-dev python-dev bzr git vim telnet
-
-RUN echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-RUN apt-get update
-RUN apt-get install -y --allow-unauthenticated postgresql-8.4
+RUN apt-get update && apt-get install -y tmux libxml2-dev libxslt1-dev python-dev libjpeg-dev libpng-dev libfreetype6-dev build-essential wget pkg-config libpq-dev python-dev bzr git vim telnet net-tools unzip netcat-openbsd xvfb
+RUN apt-get update && apt-get install -y firefox=45.0.2+build1-0ubuntu1
 
 RUN wget https://bootstrap.pypa.io/get-pip.py
 RUN python get-pip.py
 ADD requirements.txt .
 RUN pip install -r requirements.txt
 
+
+RUN git clone https://github.com/wolfcw/libfaketime.git && cd libfaketime && make && make install
+
+RUN adduser --disabled-password --gecos "" testing
+USER testing
+
+WORKDIR /home/testing
+
 RUN bzr init-repo --no-trees repo
 RUN cd repo && bzr checkout lp:unifield-server server
 RUN cd repo && bzr checkout lp:unifield-web web
 
-RUN /etc/init.d/postgresql start && \
-    runuser -l  postgres -c "psql -c \"CREATE USER unifield_dev WITH PASSWORD 'unifield_dev';\"" && \
-    runuser -l  postgres -c "psql -c \"ALTER USER unifield_dev WITH CREATEDB\"" && \
-    runuser -l  postgres -c "psql -c \"UPDATE pg_database set datallowconn = TRUE where datname = 'template0';\"" && \
-    runuser -l  postgres -c "psql -c \"UPDATE pg_database set datistemplate = FALSE where datname = 'template1';\"" && \
-    runuser -l  postgres -c "psql -c \"DROP DATABASE template1\"" && \
-    runuser -l  postgres -c "psql -c \"CREATE DATABASE template1 with template = template0 encoding = 'UTF8'\"" && \
-    runuser -l  postgres -c "psql -c \"UPDATE pg_database set datistemplate = TRUE where datname = 'template1';\" template0" && \
-    runuser -l  postgres -c "psql -c \"UPDATE pg_database set datallowconn = FALSE where datname = 'template0';\" template1"
+USER root
 
-RUN apt-get install -y net-tools
-
-VOLUME ["/output"]
+RUN echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+RUN apt-get update && apt-get install -y --allow-unauthenticated postgresql-8.4
 
 ADD docker/docker-entrypoint.sh .
-RUN chmod +x docker-entrypoint.sh
+ADD docker/root-entrypoint.sh .
+ADD docker/config.sh .
 
-# Option (1): use a separate volume
-#VOLUME ["/input"]
-
-# Option (2): import everything in the image
-RUN mkdir /input
-
-RUN mkdir /input/instances
-WORKDIR /input/instances
-#ADD instances .
-
-RUN mkdir /input/meta_features
-WORKDIR /input/meta_features
-#ADD meta_features .
-
-WORKDIR /root
-
-ENTRYPOINT ["/root/docker-entrypoint.sh"]
+ENTRYPOINT ["/home/testing/root-entrypoint.sh"]
 
 EXPOSE 8080
 EXPOSE 8003
 
-RUN apt-get install -y curl
+VOLUME ["/output"]
 
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-export LANG=C.UTF-8
-
 set -o errexit
-#set -o pipefail
 
+export LANG=C.UTF-8
 # if you want to create a RAMFS with the database
 PARAMS=$@
-
-HAS_REFRESH=`echo $PARAMS | sed -n 's/.*--refresh.*/YES/p'`
-PARAMS=`echo $PARAMS | sed 's/--refresh//'`
-
-HAS_QUICK=`echo $PARAMS | sed -n 's/.*--quick.*/YES/p'`
-PARAMS=`echo $PARAMS | sed 's/--quick//'`
-
-HAS_RELOAD=`echo $PARAMS | sed -n 's/.*--reload.*/YES/p'`
-PARAMS=`echo $PARAMS | sed 's/--reload//'`
 
 BRANCH_TOO_CLONE=master
 if [[ ! -z "`echo $PARAMS | sed -n 's/.*--branch*/YES/p'`" ]]
@@ -51,7 +40,7 @@ function make_ramfs
 
 function run_website
 {
-    cd /root/testfield/website
+    cd /home/testing/testfield/website
     cp -R /output/benchmarks/* performances/ 2> /dev/null || true
     cp -R /output/tests/* tests/ 2> /dev/null || true
     python performance.py
@@ -60,6 +49,12 @@ function run_website
 function get_repo
 {
     git clone -b $BRANCH_TOO_CLONE https://github.com/hectord/testfield.git && cd testfield
+
+    if [[ -f config.sh ]];
+    then
+        rm config.sh
+    fi
+    cp ../config.sh .
 }
 
 if [[ "$1" == "web" && $# == 1 ]];
@@ -72,8 +67,8 @@ fi
 if [[ $# -lt 2 || ( "$1" != benchmark && "$1" != "test" ) ]];
 then
     echo "Usage: "
-    echo "  $0 benchmark name [--quick] [--refresh] [--reload] [--branch=git_branch] [server_branch] [web_branch] [tag]"
-    echo "  $0 test name [--quick] [--refresh] [--reload] [--branch=git_branch] [server_branch] [web_branch] [tag]"
+    echo "  $0 benchmark name [--branch=git_branch] [server_branch] [web_branch] [tag]"
+    echo "  $0 test name [--branch=git_branch] [server_branch] [web_branch] [tag]"
     echo "  $0 web"
     exit 1
 fi
@@ -81,15 +76,13 @@ fi
 TO_PATH=
 FROM_PATH=
 
-#LETTUCE_PARAMS="${*:5}"
-
 if [[ "$1" == "benchmark" ]];
 then
     TO_PATH=/output/benchmarks/${ARRPARAMS[1]}
-    FROM_PATH=/root/testfield/website/performances/${ARRPARAMS[1]}
+    FROM_PATH=/home/testing/testfield/website/performances/${ARRPARAMS[1]}
 else
     TO_PATH=/output/tests/${ARRPARAMS[1]}
-    FROM_PATH=/root/testfield/website/tests/${ARRPARAMS[1]}
+    FROM_PATH=/home/testing/testfield/website/tests/${ARRPARAMS[1]}
 fi
 
 if [[ -e $TO_PATH ]];
@@ -100,59 +93,28 @@ fi
 
 get_repo;
 
-BACKUP_NAME=backup.tar.gz
-PATH_CACHE=/output/$BACKUP_NAME
-if [[ ! ( -z "$HAS_REFRESH" ) || ! ( -e "$PATH_CACHE" ) ]]
-then
-    if [[ -e "$PATH_CACHE" ]]
-    then
-        rm -f "$PATH_CACHE"
-    fi
+cd /home/testing/testfield
 
-    echo "Downloading the tests..."
-    curl http://uf0003.unifield.org/backup.tar.gz > $PATH_CACHE
-fi
-
-cp $PATH_CACHE $BACKUP_NAME
-gunzip $BACKUP_NAME -c > tmp.tar
-tar -xvvf tmp.tar
-
-if [[ ! ( -z "$HAS_QUICK" ) ]]
-then
-    PG_DATA_DIR=/var/lib/postgresql/8.4/main/
-    #TODO: Check that it works with PostgreSQL 8.4 ... (the path should be differnet)
-    make_ramfs $PG_DATA_DIR
-    chown -R postgres:postgres $PG_DATA_DIR
-    chmod 0700 $PG_DATA_DIR
-
-    cd /tmp
-    make_ramfs /root/testfield
-    cd /root/testfield
-fi
+./fetch/owncloud/fetch.sh
 
 # we have to create the directories for the input/output files (features & dumps)
 mkdir output || true
 
-/etc/init.d/postgresql start
-cd /root/testfield
+cd /home/testing/testfield
 
-export RELOAD_BASE_MODULE=no
-if [[ ! ( -z "$HAS_RELOAD" ) ]]
-then
-    export RELOAD_BASE_MODULE=yes
-fi
+function copy()
+{
+    if [[ -e $TO_PATH ]];
+    then
+        rm -rf $TO_PATH;
+    fi
+    mkdir $TO_PATH;
+
+    if [[ -e $FROM_PATH ]]
+    then
+        cp -R $FROM_PATH/* $TO_PATH;
+    fi
+}
+trap "copy;" EXIT;
 
 ./runtests_server.sh $PARAMS
-
-if [[ -e $TO_PATH ]]
-then
-    rm -rf $TO_PATH
-fi
-mkdir $TO_PATH
-
-
-if [[ -e $FROM_PATH ]]
-then
-    cp -R $FROM_PATH/* $TO_PATH
-fi
-
