6f21e4081963c47c29ef82693b2b47eb4230ede5
Hector Davensburg <hector.davensburg@gmail.com>
2016-06-15 10:32:51 +0200

    Change the volume permission in docker
    It's required to export data after a benchmark / tests

--- docker/root-entrypoint.sh
+++ docker/root-entrypoint.sh
@@ -5,5 +5,9 @@ set -o errexit
 Xvfb :1 -screen 0 1024x768x16 &
 export DISPLAY=:1
 
+chmod -R 777 /output
+chown -R testing:testing /output
+
 su testing -c "/home/testing/docker-entrypoint.sh $*"
+
 
