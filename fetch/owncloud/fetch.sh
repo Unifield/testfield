cbb72d7f8891f145b3ca111bc084c201edee9feb
Hector Davensburg <hector.davensburg@gmail.com>
2016-07-12 19:24:50 +0200

    Remove the need to specify the directory name when fetching data in ownCloud

--- fetch/owncloud/fetch.sh
+++ fetch/owncloud/fetch.sh
@@ -2,19 +2,26 @@
 
 set -e
 
+[[ -e .tmp ]] && rm -rf .tmp
+mkdir .tmp
+
 echo "Clean up data"
 rm -rf files meta_features instances
 
 KEY_FETCH=${KEY_FETCH-vRN2afzfa7bMOXQ}
-DIR_FETCH=${DIR_FETCH-tests}
 
 echo "Download the zip file"
 wget -O tests.zip https://cloud.msf.org/index.php/s/${KEY_FETCH}/download
+DIRNAME=$(unzip -qql tests.zip | head -n1 | tr -s ' ' | cut -d' ' -f5-)
+
+mv tests.zip .tmp/
+cd .tmp
 
 echo "Unzip"
 unzip tests.zip
 
-mv ${DIR_FETCH}/* .
-rm -rf ${DIR_FETCH} tests.zip
+cp -R $DIRNAME/* ../
 
+cd ..
+rm -rf .tmp
 
