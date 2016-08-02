0174a0b03339f91b31e4dd1f7ecb0893931b08c8
Hector Davensburg <hector.davensburg@gmail.com>
2016-08-02 09:33:32 +0200

    Change the key to download the tests (user Andres's ownCloud)

--- fetch/owncloud/fetch.sh
+++ fetch/owncloud/fetch.sh
@@ -8,7 +8,7 @@ mkdir .tmp
 echo "Clean up data"
 rm -rf files meta_features instances
 
-KEY_FETCH=${KEY_FETCH-vRN2afzfa7bMOXQ}
+KEY_FETCH=${KEY_FETCH-U1rh2XBegWVz78F}
 
 echo "Download the zip file"
 wget -O tests.zip https://cloud.msf.org/index.php/s/${KEY_FETCH}/download
