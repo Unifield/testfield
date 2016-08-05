0b9e0c813a813c978764b76cb7d84bf744cf5450
Hector Davensburg <hector.davensburg@gmail.com>
2016-08-05 17:09:02 +0200

    Remove the container's data after running testfield

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -13,7 +13,7 @@ then
     EXTRA_PARAM=\"-e KEY_FETCH=\$TESTFIELD_TEST_KEY\"
 fi
 
-sudo docker run \$EXTRA_PARAM --privileged -it -P -p 8080:8080 -v `pwd`/output:/output hectord/autotestfield \$@
+sudo docker run \$EXTRA_PARAM --rm --privileged -it -P -p 8080:8080 -v `pwd`/output:/output hectord/autotestfield \$@
 " > run.sh
 
 sudo chmod +x run.sh
