9369d3b3921709b5d2d2c9c78b8f7ee42c72a632
Kharianne <katerinamrkackova@gmail.com>
2020-08-10 00:23:58 +0200

    add faking hw id

---
+++ container_fake_hw_id.py
@@ -0,0 +1,13 @@
+import utils
+import sys
+
+hw_id = sys.argv[1]
+print("Setting hw_id to: ", hw_id)
+
+sync_db = utils.prefix_db_name('SYNC_SERVER')
+connection = utils.XMLRPCConnection(sync_db)
+ent_obj = connection.get('sync.server.entity')
+ids = ent_obj.search([])
+if len(ids) == 0:
+    raise RuntimeError("Could not find entity to edit.")
+ent_obj.write(ids, {'hardware_id': hw_id})
