b0dfcef049d62f7ea531ddbb18e074e719f4d6ad
Kharianne <katerinamrkackova@gmail.com>
2020-08-10 13:47:48 +0200

    run fake only if called with main

--- container_fake_hw_id.py
+++ container_fake_hw_id.py
@@ -1,13 +1,14 @@
 import utils
 import sys
 
-hw_id = sys.argv[1]
-print("Setting hw_id to: ", hw_id)
+if __name__ == '__main__':
+    hw_id = sys.argv[1]
+    print("Setting hw_id to: ", hw_id)
 
-sync_db = utils.prefix_db_name('SYNC_SERVER')
-connection = utils.XMLRPCConnection(sync_db)
-ent_obj = connection.get('sync.server.entity')
-ids = ent_obj.search([])
-if len(ids) == 0:
-    raise RuntimeError("Could not find entity to edit.")
-ent_obj.write(ids, {'hardware_id': hw_id})
+    sync_db = utils.prefix_db_name('SYNC_SERVER')
+    connection = utils.XMLRPCConnection(sync_db)
+    ent_obj = connection.get('sync.server.entity')
+    ids = ent_obj.search([])
+    if len(ids) == 0:
+        raise RuntimeError("Could not find entity to edit.")
+    ent_obj.write(ids, {'hardware_id': hw_id})
